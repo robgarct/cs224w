b6f3a778c81b8dc13cf4bc0d2097e813f9486be3
roberto <roberto.gt1509@outlook.com>
2023-11-25 12:45:38 -0800

    setup greedy_inference_eval

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
 from generator import *
 from model import Model
 from typing import List
+from torch_geometric.data.batch import Batch
 
 def greedy_inference_single(
     model: Model,
@@ -12,7 +13,15 @@ def greedy_inference_single(
     # TODO(roberto): What should we do here? start from 0 or not
     curr_sol = SolutionInstance(graph_wo_edges, [0])
     while not curr_sol.sol_is_complete():
-        pass
+        curr_graph = curr_sol.as_partial_solution()
+        curr_graph = curr_graph.export_pyg()
+        curr_batch = Batch.from_data_list([curr_graph])
+        next_probs = model.probs(curr_batch)
+        next_node = next_probs.squeeze().argmax().cpu().item()
+        curr_sol.add_node(next_node)
+    # TODO(roberto): Should we also be adding the 0 node at the end?
+    curr_sol.add_node(0)
+    return curr_sol
 
 def beam_inference_single(
     model: Model,
