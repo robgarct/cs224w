d1fdc9b200b14200571d65781ca62a2f9f094a93
roberto <roberto.gt1509@outlook.com>
2023-11-24 19:15:57 -0800

    add comments

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import torch
 from torch import nn
 import itertools
+import pandas as pd
 
 from model import Model
 from generator import *
@@ -15,9 +16,19 @@ from torch_geometric.loader import DataLoader
 from typing import List
 
 def get_solution_instances(graphs_path:str) -> List[SolutionInstance]:
-    pass
+    """Given the path to the pickle of graphs generated and further solved
+    using LKH, returns a list of SolutionInstances.
+    """
+    graphs = pd.read_pickle(graphs_path)
+    graphs = [(CVRPGraph(g), sol) for g, sol in graphs]
+    return [SolutionInstance(g, sol) for g, sol in graphs]
+        
 
 def get_data_loaders(graphs_path:str, train_split_size=0.8, batch_size=16) -> DataLoader:
+    """Given the path to the pickle of graphs generated and further solved
+    using LKH, returns 2 PYG's DataLoaders, one for training and the other
+    for validation.
+    """
     graphs = []
     sol_instances = get_solution_instances(graphs_path)
     split_idx = int(train_split_size * len(sol_instances))
@@ -36,9 +47,14 @@ def get_data_loaders(graphs_path:str, train_split_size=0.8, batch_size=16) -> Da
     return train_graphs, valid_graphs
 
 def compute_accuracy(logits, nexts):
+    """Given logit predictions and the ground truth nodes to be visited next,
+    computes the accuracy of the model.
+    """
     return (torch.argmax(logits) == nexts).mean().cpu().detach()
 
 def eval(model: Model, data_loder: DataLoader):
+    """Evaluates the model on the given data loader.
+    """
     loss_fn = nn.CrossEntropyLoss()
     losses = []
     accs = []
@@ -53,7 +69,9 @@ def eval(model: Model, data_loder: DataLoader):
         
 
 def train(model: Model, graphs_path: str, epochs:int = 20, batch_size:int = 16, learning_rate:float=3e-5, eval_epochs:int=1):
-    # Training loop
+    """Runs training for the model on the graphs pointed out by the given graphs path.
+    The model weights are updated inplace, so this function returns nothing.
+    """
     optimizer = Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.CrossEntropyLoss()
     train_dl, valid_dl = get_data_loaders(graphs_path, batch_size=batch_size)
